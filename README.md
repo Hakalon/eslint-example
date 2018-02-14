25385b134bf46eda1db22971d4c1988e15d5d8ea
Steve Lai <tf4ewg@gmail.com>
2018-02-14 20:04:12 +0800

    apply ESLint & Update README.md

--- README.md
+++ README.md
@@ -1,19 +1,43 @@
-# Project Name
+# ESLint-example
 
-There...
+This is a simple project for eslint.
+***
+# Running Problems
 
-### Description:
-Using alexa-app...
+* Error msg: Failed to load the ESLint library...
 
-### Description:
-In this branch...
-[link](https://google.com)
-*Strong*
+There are two possible reason, one is you don't install ESLint well, and the other is you install ESLint **globally** with **yarn**.
 
-# Tilte
+In the second case, I suggest you install ESLint **locally** no matter what you are using*(npm or yarn)*.
 
-### Description
+# ESLint Rules
 
+* [linebreak-style](https://eslint.org/docs/rules/linebreak-style)
+If you want to learn more, [here is the details](https://stackoverflow.com/questions/37826449/expected-linebreaks-to-be-lf-but-found-crlf-linebreak-style-in-eslint-using).
+* [no-console](https://eslint.org/docs/rules/no-console)
+* [no-plusplus](https://eslint.org/docs/rules/no-plusplus)
+* [no-param-reassign](https://eslint.org/docs/rules/no-param-reassign)
+Prevent reassign value to the parameters which are given by function.
+* [arrow-parens](https://eslint.org/docs/rules/arrow-parens)
+* [comma-dangle](https://eslint.org/docs/rules/comma-dangle)
+* [object-shorthand](https://eslint.org/docs/rules/object-shorthand)
+It was how you should defind an object before ES6:
 ```=bash
-Code or Command ...
+var foo = {
+    w: function() {},
+    x: function *() {},
+    [y]: function() {},
+    z: z
+};
 ```
+It is how you should defind an object after ES6:
+```=bash
+var foo = {
+    w() {},
+    *x() {},
+    [y]() {},
+    z
+};
+```
+Be noticed that when you only give a propery value, then the name of that property is the name of that parameter.
+(like 'z' property) 
---
+++ index.js
@@ -0,0 +1,174 @@
+const express = require('express');
+const alexa = require('alexa-app');
+const fs = require('fs');
+const reqSender = require('request-promise-native');
+const bodyParser = require('body-parser');
+const https = require('https');
+const pad = require('left-pad');
+
+const expressApp = express();
+
+// Set the server address at https://.../demo
+// ESLlint: new-cap, requires all new operators to be called with uppercase-started functions
+const app = new alexa.app('emo');
+
+// Express server's Cert.
+const SERVER_CONFIG = {
+  key: fs.readFileSync('privkey.pem'),
+  cert: fs.readFileSync('cert.pem'),
+  ca: fs.readFileSync('chain.pem'),
+};
+
+
+function reqErrHandler(err) {
+  return new Promise((resolve, reject) => {
+    console.log('reqErrHandler');
+    if (err.error && typeof (err.error) === 'string') {
+      err = JSON.parse(err.error).error;
+      err.name = 'RequestError';
+      return reject(err);
+    }
+    return reject(new Error('err is undefined'));
+  });
+}
+
+function checkResp(count = 0) {
+  return new Promise((resolve, reject) => {
+    console.log('checkResp');
+    reqSender.get('https://openhab.ntust.ml/rest/items/ResponseEchoMessage')
+      .then(body => {
+        // ESLint: no-param-reassign 禁止賦值給函式給予的變數('body'為函式給予的變數)
+        // 但這邊會讓它通過，因為在這Promise中我只是將body重新轉化為Json格式
+        if (body && typeof (body) === 'string') { body = JSON.parse(body); }
+        if (body.state) { return resolve(body.state); }
+        if (count > 10) {
+          const err = new Error('checkResp recurs too much times!');
+          err.name = 'checkResp';
+          return reject(err);
+        }
+        console.log(`checkResp recurs ${count + 1} times`);
+        //
+        count++;
+        return resolve(checkResp(count));
+      })
+      .catch(reqErrHandler);
+  });
+}
+
+function cleanResp(resp) {
+  return new Promise((resolve, reject) => {
+    console.log('cleanResp');
+    reqSender.post({
+      url: 'https://openhab.ntust.ml/rest/items/ResponseEchoMessage',
+      body: '',
+    })
+      .then(() => resolve(resp))
+      .catch(reqErrHandler);
+    // Line 59 fix
+    // .then(() => reject());
+  });
+}
+
+function finalErrHandler(err) {
+  console.log('finalErrHandler');
+  console.log(`Error name : ${err.name}`);
+  console.log(`Error message : ${err.message}`);
+  return Promise.resolve('Sorry, It seems something goes wrong. Please check the server log.');
+}
+
+
+app.intent('HelloWorld', (request, response) => {
+  console.log('HelloWorld intent called');
+  response.say('Hello World');
+});
+
+app.intent('Airconditioner', (request, response) => {
+  console.log('Airconditioner Intent Called');
+
+  return reqSender.post({
+    url: 'https://openhab.ntust.ml/rest/items/ReceivedEchoMessage',
+    body: 'Airconditioner',
+  })
+    .catch(reqErrHandler)
+    .then(checkResp)
+    .then(cleanResp)
+    .catch(finalErrHandler)
+    // 下方這段Code會因 ESLint: arrow-body-style 而報錯，正確修改後為下方註解程式碼
+    // 但因此段Promise回傳在alexa-app函式內，故需加上return關鍵字(理論上確實下方註解才是最好的寫法，但實際測試卻不work)
+    .then(resp => {
+      return response.say(resp);
+    });
+  // .then(resp => response.say(resp));
+});
+
+app.intent('ACCondition', (request, response) => {
+  console.log('ACCondition Intent Called');
+
+  return reqSender.post({
+    url: 'https://openhab.ntust.ml/rest/items/ReceivedEchoMessage',
+    body: 'ACCondition'
+  })
+    .catch(reqErrHandler)
+    .then(checkResp)
+    .then(cleanResp)
+    .catch(finalErrHandler)
+    .then(resp => {
+      return response.say(resp);
+    });
+});
+
+
+// attach app with Express server
+app.express({
+  expressApp,
+});
+
+expressApp.use(bodyParser.json());
+
+expressApp.get('/', (req, resp) => {
+  console.log('index.js is called');
+  resp.status(200).json({
+    STATUS: 'OK'
+  });
+});
+
+expressApp.get('/test', (req, resp) => {
+  resp.status(200).json({
+    STATUS: 'test'
+  });
+});
+
+/**
+ * Handle Alexa request
+ */
+expressApp.post('/ask', (req, resp) => {
+  /**
+   * Verifirer (undone)
+   */
+  // if(req.body.session.application.applicationId != )
+
+  const intentName = req.body.request.intent.name;
+  console.log(`${intentName} is called`);
+
+  /**
+   * Custom Response
+   */
+  resp.json({
+    version: '1.0',
+    response: {
+      outputSpeech: {
+        type: 'SSML',
+        ssml: '<speak> Alexa is a bad girl. </speak>'
+      },
+      shouldEndSession: true
+    },
+    sessionAttributes: {}
+  });
+});
+
+https.createServer(SERVER_CONFIG, expressApp)
+  .listen(443, () => {
+    console.log('========================================');
+    console.log(pad('HTTPS sever 啟動', 25));
+    console.log('========================================');
+  });
